4f611be7c4fef101c49ae20a691af1661ec7c740
sanjanayadav077 <sanjana.28472@stu.upes.ac.in>
2025-10-03 13:01:06 +0530

    Add files via upload

---
+++ Days_21_B_perfect_number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int number,sum=0;
+    printf("enter number");
+    scanf("%d",&number);
+    for(int i=1;i<=number/2;i++){
+        if(number%i==0){
+            sum=sum+i;
+        }
+    }
+    if(sum==number && number!=0){
+        printf("%d is a perfect number",number);
+    }
+    else{
+        printf("%d is not a perfect number",number);
+    }
+    return 0;
+    
+}
