001eb2441a405772f5723d7acbbb80cc13439613
sanjanayadav077 <sanjana.28472@stu.upes.ac.in>
2025-10-05 10:56:52 +0530

    Add files via upload

---
+++ Days_27_A_star_diamond.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int space=1;space<=n-i;space++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=n-1;i>=0;i--){
+        for(int space=1;space<=n-i;space++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+    
+
+}
