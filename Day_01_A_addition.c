6295b1a28c455ed2426d0c26ef273d4291c7f4a2
sanjanayadav077 <sanjana.28472@stu.upes.ac.in>
2025-11-24 18:10:55 +0530

    Add files via upload

---
+++ Day_01_A_addition.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b ,sum;
+    printf("enter your number");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("sum of two number %d",sum);
+    return 0;
+}
